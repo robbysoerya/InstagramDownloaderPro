b6d4a0706ea7b249d8716211b8ed04c4dc90c23a
robbysoerya <robbysoerya6@gmail.com>
2018-07-28 07:05:42 +0700

    Remove ADS

--- app/src/main/java/com/massurya/instagramdownloaderpro/SettingsActivity.java
+++ app/src/main/java/com/massurya/instagramdownloaderpro/SettingsActivity.java
@@ -22,7 +22,6 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreference()).commit();
 
     }
